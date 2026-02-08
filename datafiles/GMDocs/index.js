eb9fba1b07db629960d28c9a06aa74dc43f722b3
WuffMakesGames <56092018+WuffMakesGames@users.noreply.github.com>
2026-02-08 14:49:24 -0500

    Generation templates

--- datafiles/GMDocs/index.js
+++ datafiles/GMDocs/index.js
@@ -1,13 +1,19 @@
+// Collapsible items
 var collapsible = document.getElementsByClassName("collapsible");
 for (var i = 0; i < collapsible.length; i++) {
+	collapsible[i].getElementsByTagName("img")[0].src = "images/arrow-right.png";
 	collapsible[i].addEventListener("click", function() {
 		this.classList.toggle("active");
 
 		var content = this.nextElementSibling;
+		var icon = this.getElementsByTagName("img")[0];
+		
 		if (content.style.maxHeight) {
 			content.style.maxHeight = null;
+			icon.src = "images/arrow-right.png";
 		} else {
 			content.style.maxHeight = "max-content";
+			icon.src = "images/arrow-down.png";
 		}
 	});
 }
